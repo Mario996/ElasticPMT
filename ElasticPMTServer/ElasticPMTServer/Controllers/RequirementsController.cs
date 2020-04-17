ffc89d24606530a4613e6af41b21a4022c58fe18
Mario996 <klisanicmario@gmail.com>
2020-04-17 12:47:10 +0200

    Added correct project files

---
+++ ElasticPMTServer/ElasticPMTServer/Controllers/RequirementsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElasticPMTServer.Models;
+using ElasticPMTServer.Services;
+using Elasticsearch.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticPMTServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RequirementsController : ControllerBase
+    {
+
+        private readonly IElasticSearchService _elasticSearchService;
+
+        public RequirementsController(IElasticSearchService elasticSearchService)
+        { 
+            _elasticSearchService = elasticSearchService;
+        }
+
+        // GET: api/Requirements
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Requirements/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: requirements
+        [HttpPost]
+        public IActionResult createRequirement([FromBody] Requirement requirement)
+        {
+            var result = _elasticSearchService.createRequirement(requirement);
+            return StatusCode(result.HttpStatusCode.Value);
+        }
+
+        // PUT: api/Requirements/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ElasticPMTServer/ElasticPMTServer/Services/IElasticSearchService.cs
@@ -0,0 +1,14 @@
+﻿using ElasticPMTServer.Models;
+using Elasticsearch.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticPMTServer.Services
+{
+    public interface IElasticSearchService
+    {
+          StringResponse createRequirement(Requirement requirement);
+    }
+}
